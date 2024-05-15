783c2fdc57eebfdcea4c70d0647a6a1a22575736
Zach Farrow <zachary.b.farrow@gmail.com>
2024-05-15 13:34:45 -0400

    prompt for userId in each instance

--- frontend/src/WebSocketDemo.tsx
+++ frontend/src/WebSocketDemo.tsx
@@ -3,18 +3,22 @@ import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { css } from '@emotion/react';
 
 export const WebSocketDemo = () => {
-  const [value, setValue] = useState<Record<string, string>>({ A: '', B: '' });
-  const [sender, setSender] = useState('');
+  const [value, setValue] = useState('');
+  const [userId, setUserId] = useState('');
   const [newMsg, setNewMsg] = useState(false);
   const [response, setResponse] = useState<Record<string, string>[]>([{ user: '', data: '' }]);
 
   const ws = useRef<WebSocket | null>(null);
+  const userIdRef = useRef<HTMLInputElement>();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newValue = { ...value };
-    newValue[e.target.id] = e.target.value;
-    setValue({ ...newValue });
-    setSender(e.target.id);
+    setValue(e.target.value);
+  };
+
+  const handleSubmitUserId = () => {
+    if (userIdRef && userIdRef.current) {
+      setUserId(userIdRef.current.value);
+    }
   };
 
   useEffect(() => {
@@ -38,9 +42,9 @@ export const WebSocketDemo = () => {
   }, []);
 
   useEffect(() => {
-    if (!ws.current) return;
+    if (!ws.current || !userId) return;
 
-    const apiCall = { user: sender, data: value[sender] };
+    const apiCall = { user: userId, data: value };
 
     if (ws.current) {
       ws.current.onopen = () => {
@@ -49,7 +53,7 @@ export const WebSocketDemo = () => {
         }
       };
       ws.current.onopen(event as Event);
-      setValue({ A: '', B: '' });
+      setValue('');
     }
   }, [newMsg]);
 
@@ -65,14 +69,7 @@ export const WebSocketDemo = () => {
       `}
     >
       <Typography variant='h5'>Websocket Chat Example</Typography>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          width: 70vw;
-        `}
-      >
+      {!userId ? (
         <div
           css={css`
             display: flex;
@@ -83,45 +80,65 @@ export const WebSocketDemo = () => {
           `}
         >
           <TextField
-            label='Chat Message from User A'
+            label='Enter user id'
             css={css`
               background-color: white;
               width: 50%;
               margin: 1em;
             `}
-            onChange={handleChange}
-            value={value.A}
-            id='A'
+            inputRef={userIdRef && userIdRef}
+            autoFocus
+            autoComplete='off'
           />
-          <Button variant='contained' onClick={() => setNewMsg((prev) => !prev)}>
-            Send from User A
+          <Button variant='contained' onClick={handleSubmitUserId}>
+            Submit
           </Button>
         </div>
+      ) : (
         <div
           css={css`
             display: flex;
-            flex-direction: row;
-            width: 100%;
+            flex-direction: column;
             align-items: center;
-            justify-content: center;
+            width: 70vw;
           `}
         >
-          <TextField
-            label='Chat Message from User B'
+          <div
             css={css`
-              background-color: white;
-              width: 50%;
-              margin: 1em;
+              display: flex;
+              flex-direction: row;
+              width: 100%;
+              align-items: center;
+              justify-content: center;
             `}
-            onChange={handleChange}
-            value={value.B}
-            id='B'
-          />
-          <Button variant='contained' onClick={() => setNewMsg((prev) => !prev)}>
-            Send from User B
-          </Button>
+          >
+            <TextField
+              label='Press enter or click the button to send a message...'
+              css={css`
+                background-color: white;
+                width: 50%;
+                margin: 1em;
+              `}
+              onChange={handleChange}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  setNewMsg((prev) => !prev);
+                }
+              }}
+              value={value}
+              autoFocus
+              autoComplete='off'
+            />
+            <Button
+              variant='contained'
+              onClick={() => setNewMsg((prev) => !prev)}
+              disabled={!userId || !value}
+            >
+              Send
+            </Button>
+          </div>
         </div>
-      </div>
+      )}
       {response && (
         <div
           css={css`
@@ -132,25 +149,60 @@ export const WebSocketDemo = () => {
             if (msg.user && msg.data)
               return (
                 <div
+                  key={msg.data}
                   css={
-                    msg.user === 'A'
+                    msg.user === userId
                       ? css`
                           display: flex;
-                          flex-direction: row;
+                          flex-direction: column;
+                          height: 100%;
                           justify-content: flex-start;
-                          color: blue;
+                          align-items: start;
                         `
                       : css`
                           display: flex;
-                          flex-direction: row;
-                          justify-content: flex-end;
-                          color: green;
+                          flex-direction: column;
+                          height: 100%;
+                          justify-content: flex-start;
+                          align-items: end;
                         `
                   }
                 >
                   <Typography
-                    key={`${msg.user}-${msg.data}`}
-                  >{`${msg.user}: ${msg.data}`}</Typography>
+                    css={css`
+                      padding-bottom: 0.25em;
+                      padding-left: 0.5em;
+                      padding-right: 0.5em;
+                      width: fit-content;
+                    `}
+                    variant='caption'
+                  >
+                    {msg.user === userId ? 'You' : msg.user}
+                  </Typography>
+                  <span
+                    css={
+                      msg.user === userId
+                        ? css`
+                            color: white;
+                            background-color: steelblue;
+                            border: solid black 1px;
+                            border-radius: 10%;
+                            width: fit-content;
+                            padding: 0.25em 1em;
+                            margin-bottom: 0.5em;
+                          `
+                        : css`
+                            color: white;
+                            background-color: green;
+                            border: solid black 1px;
+                            border-radius: 10%;
+                            width: fit-content;
+                            padding: 0.25em 1em;
+                          `
+                    }
+                  >
+                    <Typography variant='body1' key={`${msg.data}`}>{`${msg.data}`}</Typography>
+                  </span>
                 </div>
               );
           })}
